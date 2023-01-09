7b6be000e47bf00c29dbfc8643e62c3f716a046c
Svetlana-Dehtiarova <degsvet1977@gmail.com>
2023-01-09 20:57:28 +0200

    Update task01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
-console.log(`Hello`)
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+let intervalId = null;
+buttonStop.setAttribute('disabled', '');
+
+buttonStart.addEventListener('click', onButtonStartClickChangeColor);
+buttonStop.addEventListener('click', onButtonStopClick);
+
+function onButtonStartClickChangeColor() {
+  buttonStart.setAttribute('disabled', true);
+  buttonStop.removeAttribute('disabled');
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onButtonStopClick() {
+  buttonStop.setAttribute('disabled', true);
+  buttonStart.removeAttribute('disabled');
+  clearInterval(intervalId);
+}
